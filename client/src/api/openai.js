40969b46a3e2e83ea0bce7bbeae7591d976034d5
Name Anna Starenka <starenka.anna.zoolux@gmail.com>
2025-07-18 14:03:53 +0300

    build after changr openAi

--- client/src/api/openai.js
+++ client/src/api/openai.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL =' https://lotr-chat-server.onrender.com'|| 'http://localhost:3001'
+const API_URL ='https://lotr-chat-server.onrender.com'|| 'http://localhost:3001'
 
 export async function sendMessage(messages, character) {
   const response = await axios.post(`${API_URL}/chat`, { messages, character })
